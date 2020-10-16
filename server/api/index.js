1cedcc7e56a6686cf85dcb81b9e9f8020fb46720
Mayur Varma <mayurvarma14@gmail.com>
2020-10-16 21:14:19 +0530

    refactor:clean routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
-const jsyaml = require('js-yaml');
 
+const swaggerDoc = require('../config/documentation.json');
 const userRoutes = require('./user/config/routes');
 const movieRoutes = require('./movie/config/routes');
 const genreRoutes = require('./genre/config/routes');
@@ -16,13 +13,10 @@ router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
 router.use('/genres', genreRoutes);
 
-// const spec = fs.readFileSync(path.join(__dirname, 'swagger.yaml'), 'utf8');
-// const swaggerDoc = jsyaml.safeLoad(spec);
-
 router.use(
   '/docs',
   swaggerUi.serve,
-  swaggerUi.setup(require('../config/documentation.json'), { explorer: true })
+  swaggerUi.setup(swaggerDoc, { explorer: true })
 );
 
 module.exports = { routes: router, user: { service: userService } };
